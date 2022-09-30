8eabc9214dca05d635323842f6f3f85649069baa
Lilkizarmie <jefferyadolor18@gmail.com>
2022-09-30 17:51:06 +0100

    Fixed assets folder dir

--- App.js
+++ App.js
@@ -15,12 +15,17 @@ const theme = {
   }
 }
 
+
 const App = () => {
   const [loaded] = useFonts({
     InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-    InterSemiBols: require("./assets/fonts/Inter-SemiBold.ttf"),
-    
-  })
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null;
 
   return (
     <NavigationContainer theme={theme}>
